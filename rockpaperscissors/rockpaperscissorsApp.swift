cc6d653106fd5c0ff8ce8f42587bbd55d2664e2b
Neeta Buhecha <neeta.b@gmail.com>
2024-05-07 09:51:02 +0100

    Initial Commit

---
+++ rockpaperscissors/rockpaperscissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockpaperscissorsApp.swift
+//  rockpaperscissors
+//
+//  Created by Neeta Buhecha on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct rockpaperscissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
